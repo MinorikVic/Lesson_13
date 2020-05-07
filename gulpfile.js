b614c468e6e0a6401fdd624e4d06c6dd2ec21353
MinorikVic <pobeda1105.94@gmail.com>
2020-05-07 14:52:23 +0300

    sass

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const {src, dest, watch} = require('gulp');
+const browserSync = require('browser-sync').create();
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+
+// Static server
+function bs() {
+    serveSass();
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    watch('./*.html').on('change', browserSync.reload);
+    watch('./sass/**/*.sass', serveSass).on('change', browserSync.reload);
+    watch('./js/*.js').on('change', browserSync.reload);
+};
+
+function mincss() {
+    src('src/**/*.css')
+        .pipe(cssmin())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(dest('dist'));
+};
+function serveSass() {
+    return src("./sass/*.sass")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+exports.serve = bs
