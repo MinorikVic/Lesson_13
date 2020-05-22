06db2b617285e4c2710ec3d115bebd2de69f201e
MinorikVic <pobeda1105.94@gmail.com>
2020-05-22 17:52:28 +0300

    validate

--- js/main.js
+++ js/main.js
@@ -44,4 +44,40 @@ $(document).ready(function () {
     bullets.css('left', prev.width() + 25)
 
     new WOW().init();
+
+    //Валидация формы
+
+    $('.modal__form').validate({
+        errorClass: "invalid",
+        rules: {
+            // simple rule, converted to {required:true}
+            userName: {
+                required: true,
+                minlength: 2,
+                maxlength: 15
+            },
+            userPhone: "required",
+            // compound rule
+            userEmail: {
+              required: true,
+              email: true
+            }
+          },
+        messages: {
+            userName: {
+                required: "Заполните поле",
+                minlength: "Не менее 2 букв",
+                maxlength: "Не более 15 букв"
+            },
+            userPhone: "Телефон обязателен",
+            userEmail: {
+              required: "Обязательно укажите email",
+              email: "Введите корректный email"
+            }
+          }
+    });
+
+    //маска для телефона
+
+    $('[type=tel]').mask('+7(000)000-00-00', {placeholder: "+7 (___) ___-__-__"});
 });
